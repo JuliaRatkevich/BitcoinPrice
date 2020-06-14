aafa3ea8ff3bd8ed30bc3b8dd641242983d6942d
Julia <juliakuntsevich@gmail.com>
2020-06-14 16:41:56 +0200

    add initial table view

--- BitcoinPrice/BitcoinPrice/ViewController.swift
+++ BitcoinPrice/BitcoinPrice/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupTable()
     }
-
-
+    
+    private func setupTable() {
+        table.dataSource = self
+        table.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 2
+        case 2:
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "section \(section + 1)"
+//        switch section {
+//        case 0:
+//            return "section 1"
+//        case 1:
+//            return "section 2"
+//        case 2:
+//            return "section 3"
+//        default:
+//            return nil
+//        }
+    }
 }
-
